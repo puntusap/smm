9fb27b6bf160ae0f261f08fc67ad3401f9c3bdb6
puntusap <puntus99_99@mail.ru>
2019-10-21 10:22:03 +0700

    first commit

--- app/Test/Facedes/TestFacades.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-namespace app\Test\Facades;
-use Illuminate\Support\Facades\Facade;
- 
-class TestFacades extends Facade{
-  protected static function getFacadeAccessor() { return 'test'; }
-}
